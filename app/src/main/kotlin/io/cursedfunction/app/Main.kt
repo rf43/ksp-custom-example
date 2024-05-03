9de42e6722115b026407594912de841365b5c304
CursedFunction <cursedfunction@gmail.com>
2024-05-02 22:09:20 -0400

    Add app module

--- app/src/main/kotlin/io/cursedfunction/app/Main.kt
+++ app/src/main/kotlin/io/cursedfunction/app/Main.kt
@@ -1,4 +1,4 @@
-package io.cursedfunction
+package io.cursedfunction.app
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,6 @@ plugins {
     kotlin("jvm") version "1.9.23"
 }
 
-group = "io.cursedfunction"
-version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
 }
