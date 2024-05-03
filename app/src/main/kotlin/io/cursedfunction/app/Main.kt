7155476e7e53e26205d0b37029205801503fcd87
CursedFunction <cursedfunction@gmail.com>
2024-05-03 00:46:29 -0400

    Add comments describing the output

--- app/src/main/kotlin/io/cursedfunction/app/Main.kt
+++ app/src/main/kotlin/io/cursedfunction/app/Main.kt
@@ -3,7 +3,16 @@ package io.cursedfunction.app
 import io.cursedfunction.annotation.mockme.MockingText
 
 fun main() {
+    /*
+     * The output of this code will be:
+     * ThIs iS SoMe sErIoUs tExT
+     */
     println(ThisIsSerious("This is some serious text", 43).mockMe())
+
+    /*
+     * The output of this code will be:
+     * ThIs iS AlSo vErY SeRiOuS TeXt!!!
+     */
     println(ThisIsAlsoSerious("This is also very serious text!!!").mockMe())
 }
 
