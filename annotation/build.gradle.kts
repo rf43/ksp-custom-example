8a8f4de2d0afc0cff043bc7885af2fad8a6a465c
CursedFunction <cursedfunction@gmail.com>
2024-05-02 22:16:36 -0400

    Add KSP to project build files

--- annotation/build.gradle.kts
+++ annotation/build.gradle.kts
@@ -1,15 +1,19 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20"
 }
 
 group = "io.cursedfunction.annotation"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    gradlePluginPortal()
     mavenCentral()
 }
 
-dependencies { }
+dependencies {
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.9.23-1.0.20")
+}
 
 kotlin {
     jvmToolchain(17)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20"
 }
 
 group = "io.cursedfunction"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    gradlePluginPortal()
     mavenCentral()
 }
 
